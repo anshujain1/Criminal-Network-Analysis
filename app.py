1e0d272bbfb05b939331e256101515a37d863bbf
rohanroy1 <rohanroy8345@gmail.com>
2026-04-21 02:50:07 +0530

    Add initial streamlit dashboard

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Network Link Analysis",
+    layout="wide"
+)
+
+st.title("Criminal Network Link Analysis")
+
+st.caption(
+    "All data shown is synthetically generated for demonstration purposes only."
+)
